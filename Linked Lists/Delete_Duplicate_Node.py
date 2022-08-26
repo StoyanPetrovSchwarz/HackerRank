84c01af9da3156fdeac32ec63d7f3d25c7a4a6b4
Stoyan.Petrov <Stoyan.Petrov@mail.schwarz>
2022-08-26 17:37:17 +0300

    Linked Lists Tasks Part 2

---
+++ Linked Lists/Delete_Duplicate_Node.py
@@ -0,0 +1,77 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+        self.tail = node
+
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+
+def removeDuplicates(llist):
+    # Write your code here
+
+    test_set = set()
+    second_list = SinglyLinkedList()
+
+    while llist:
+        if llist.data not in test_set:
+            test_set.add(llist.data)
+            second_list.insert_node(llist.data)
+            llist = llist.next
+        else:
+            llist = llist.next
+    return second_list.head
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        llist1 = removeDuplicates(llist.head)
+
+        print_singly_linked_list(llist1, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
